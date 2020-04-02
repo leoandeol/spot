6e094f1a93c1f57e7af75de58d65b2aed260a813
Lothair KIZARDJIAN <lothair.kizardjian@laposte.net>
2020-04-02 20:01:38 +0200

    kyo wa ijou desu

--- fist.py
+++ fist.py
@@ -1,19 +1,36 @@
 
 
+def cost(x,y):
+    z = x-y
+    return z*z
 
-
-
-def nn(X,Y):
-    return ((X[:, :, None] - Y[:, :, None].T) ** 2).sum(1).argmin(1)
-
-
+# Paper's implementation of the nn in 1-d
+def nn_paper(X,Y, start0, end0, start1, end1, assignment):
+    cursor = start1
+    for i in range(start0, end0):
+        mind = numpy.inf
+        minj = -1
+        for j in range(max(start1,cursor), end1):
+            d = cost(X[i],Y[i])
+            cursor = j-1
+            if(d <= mind):
+                mind = d
+                minj = j
+            else:
+                if(d > mind+(1e-7)):
+                    break;
+        assignment[i] = minj;
 
 def assignment(X,Y,t):
     a = []
     m = X.shape[0]
     n = Y.shape[1]
+    a.append(t[0])
     for mp in range(1,m):
-        if t[mp+1] > 
+        if t[mp+1] > a[mp]:
+            a.append(t[mp+1])
+        else:
+            
 
 def __main__():
     
